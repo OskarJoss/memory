f92f41762c73d380a045d0735f2195e1a0efd708
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-12-01 14:26:54 +0100

    added basic structure, deleted tests

---
+++ public/main.js
@@ -0,0 +1,16 @@
+
+
+//shuffle array function
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+
+
