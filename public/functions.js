a4a356014fe11d0ec91414718e6634a19e28bcb0
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-12-05 12:51:51 +0100

    added the bomb

--- public/functions.js
+++ public/functions.js
@@ -11,58 +11,66 @@ function shuffle(a) {
 }
 
 //create cards and add them to the cards-container
-const createCards = (numberOfPairs) => {
+const createCards = numberOfPairs => {
     //create an array of all the numbers on the cards
     const numbers = [];
     for (let i = 1; i <= numberOfPairs; i++) {
         numbers.push(i);
     }
     //add the numbers to the array again to create pairs
-    numbers.forEach((number) => {
+    numbers.forEach(number => {
         numbers.push(number);
-    })
+    });
 
     shuffle(numbers);
     //create the cards
-    numbers.forEach((number) => {
-        const cardFrame = document.createElement('div');
-        cardFrame.classList.add('card-frame');
-        cardFrame.innerHTML =
-        `<div class="card" data-number="${number}">
+    numbers.forEach(number => {
+        const cardFrame = document.createElement("div");
+        cardFrame.classList.add("card-frame");
+
+        if (number == numberOfPairs) {
+            cardFrame.innerHTML = `<div class="card" data-number="0">
+            <div class="card-back"></div>
+            <div class="card-front">
+                <img class="bomb-image" src="https://image.flaticon.com/icons/svg/112/112683.svg" alt="bomb">
+            </div>
+        </div>`;
+        } else {
+            cardFrame.innerHTML = `<div class="card" data-number="${number}">
             <div class="card-back"></div>
             <div class="card-front">
                 <h2>${number}</h2>
             </div>
         </div>`;
-        //add card to the cards-container
-        document.querySelector('.cards-container').appendChild(cardFrame);
-    })
-}
+        }
+
+        document.querySelector(".cards-container").appendChild(cardFrame);
+    });
+};
 
 //remove all classes of an element
-const removeAllClasses = (element) => {
+const removeAllClasses = element => {
     while (element.classList.length !== 0) {
         element.classList.remove(element.classList[0]);
     }
-}
+};
 
-const startGame = (numberOfPairs) => {
-    const container = document.querySelector('.cards-container');
+const startGame = numberOfPairs => {
+    const container = document.querySelector(".cards-container");
     //empty card container
     container.innerHTML = "";
 
     //adjust container width depending on number of pairs
     removeAllClasses(container);
-    container.classList.add('cards-container');
+    container.classList.add("cards-container");
     //change to switch statement
     if (numberOfPairs == 8) {
-        container.classList.add('easy-container');
+        container.classList.add("easy-container");
     } else if (numberOfPairs == 9) {
-        container.classList.add('medium-container');
+        container.classList.add("medium-container");
     } else if (numberOfPairs == 10) {
-        container.classList.add('hard-container');
+        container.classList.add("hard-container");
     }
 
     createCards(numberOfPairs);
-}
-
+};
